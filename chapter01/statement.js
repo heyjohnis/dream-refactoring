6e7faa28270b03c34f031ddb6138c42114528c4b
heyjohnis <juni0227@nate.com>
2021-05-03 00:01:18 +0900

    ch01 : totalAmount, totalVolumeCredits 데이터에 포함

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -1,62 +1,42 @@
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  return renderPlainText(statementData);
-
+  
+  return renderPlainText(createStatementData(invoice, plays));
+  
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
+  
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
     return result;
   }
-
+  
   function playFor(aPerformance){
     return plays[aPerformance.playID];
   }
   
-  function amountFor(aPerformance) {
+  function totalVolumeCredits(data) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(aPerformance)}`);
+    for (let perf of data.performances) {
+      // add volume credits
+      result += volumeCreditsFor(perf);
     }
     return result;
   }
-}
-
-
-function renderPlainText(data) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    // print line for this order
-    result += ` ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience} seats)\n`;
-  }
-    
-  result += `총액: ${usd(totalAmount()/100)}\n`; 
-  // 변수 인라인하기 : 변수제거하기 let volumeCredits = 0;
-  result += `적립 포인트: ${totalVolumeCredits()} credits\n`;
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber);
+  
+  function totalAmount(data) {
+    let account = 0;
+    for (let perf of data.performances) {
+      account += perf.amount;
+    }
+    return account;
   }
   
   function volumeCreditsFor(aPerf) {
@@ -68,22 +48,51 @@ function renderPlainText(data) {
     return volume;
   }
   
-  function totalVolumeCredits() {
+  function amountFor(aPerformance) {
     let result = 0;
-    for (let perf of data.performances) {
-      // add volume credits
-      result += volumeCreditsFor(perf);
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance)}`);
     }
     return result;
   }
-  
-  function totalAmount() {
-    let account = 0;
-    for (let perf of data.performances) {
-      account += perf.amount;
-    }
-    return account;
+
+}
+
+
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    // print line for this order
+    result += ` ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience} seats)\n`;
   }
+    
+  result += `총액: ${usd(data.totalAmount/100)}\n`; 
+  // 변수 인라인하기 : 변수제거하기 let volumeCredits = 0;
+  result += `적립 포인트: ${data.totalVolumeCredits} credits\n`;
+  return result;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber);
+  }
+  
 }
 
 
