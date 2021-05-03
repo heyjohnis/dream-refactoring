84f159b503ddc2d34693dab9af78a2cef16fe4cb
heyjohnis <juni0227@nate.com>
2021-05-03 23:06:18 +0900

    ch01 : refactoring complete

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -14,37 +14,30 @@ export default function createStatementData(invoice, plays) {
     return result;
   }
 
-  function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
-  }
-  
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      // add volume credits
-      result += volumeCreditsFor(perf);
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data) {
-    let account = 0;
-    for (let perf of data.performances) {
-      account += perf.amount;
-    }
     return data.performances
       .reduce((total, p) => total + p.amount,0);
   }
 
-  function volumeCreditsFor(aPerf) {
-    return new PerformanceCalculator(aPerf, playFor(aPerf)).volumeCredits;
-  }
-
   return result;
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch(aPlay.type) {
+    case "tragedy" : return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+    default :
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 class PerformanceCalculator {
@@ -54,36 +47,36 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
-
+    throw new Error('subclass responsibility');
   }
 
   get volumeCredits() {
-    let result = 0;
-    result = Math.max(this.performance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
+    return Math.max(this.performance.audience - 30, 0); 
+  }
+  
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
     return result;
   }
 
-
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
 }
