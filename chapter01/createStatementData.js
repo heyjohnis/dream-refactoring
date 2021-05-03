8e9153b4067a470d3409c88638c7ea804696b159
heyjohnis <juni0227@nate.com>
2021-05-03 21:44:11 +0900

    ch01 : class PerformanceCalculator

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -6,13 +6,18 @@ export default function createStatementData(invoice, plays) {
   result.totalVolumeCredits = totalVolumeCredits(result);
 
   function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
+  function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay);
+  }
+  
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -36,36 +41,49 @@ export default function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerf) {
-    let result = 0;
-    result = Math.max(aPerf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === aPerf.play.type)
-      result += Math.floor(aPerf.audience / 5);
-    return result;
+    return new PerformanceCalculator(aPerf, playFor(aPerf)).volumeCredits;
   }
 
-  function amountFor(aPerformance) {
+  return result;
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay
+  }
+
+  get amount() {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (this.play.type) {
       case "tragedy":
         result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
         }
-        result += 300 * aPerformance.audience;
+        result += 300 * this.performance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance)}`);
+        throw new Error(`unknown type: ${this.play.type}`);
     }
     return result;
 
   }
-  return result;
+
+  get volumeCredits() {
+    let result = 0;
+    result = Math.max(this.performance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+
 
 }
